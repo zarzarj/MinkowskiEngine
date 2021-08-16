4f7451963af8d29051f7b86ca54d9d6949baaa06
chuchis <jesus.zarzar@utexas.edu>
2021-08-16 17:53:05 +0300

    Added input pos encoding

--- examples/ScanNetLightning.py
+++ examples/ScanNetLightning.py
@@ -182,6 +182,9 @@ class ScanNet(LightningDataModule):
             self.labels = [None]*len(self.scan_files)
             if self.use_implicit_feats:
                 self.implicit_feats = [None]*len(self.scan_files)
+
+        if self.use_coord_pos_encoding:
+            self.embedder, _ = get_embedder(self.coord_pos_encoding_multires)
     
     def train_dataloader(self):
         train_dataloader = DataLoader(self.train_idx, collate_fn=self.convert_batch,
@@ -295,6 +298,8 @@ class ScanNet(LightningDataModule):
             feats.append(input_dict['coords'])
         if self.use_implicit_feats:
             feats.append(input_dict['implicit_feats'])
+        if self.use_coord_pos_encoding:
+            feats.append([self.embedder(coord) for coord in input_dict['coords']])
         out_feats = []
         for i in range(len(feats[0])):
             cur_all_feats = [feat[i] for feat in feats]
@@ -387,6 +392,8 @@ class ScanNet(LightningDataModule):
         parser.add_argument("--use_implicit_feats", type=str2bool, nargs='?', const=True, default=False)
         parser.add_argument("--use_coords", type=str2bool, nargs='?', const=True, default=False)
         parser.add_argument("--use_colors", type=str2bool, nargs='?', const=True, default=True)
+        parser.add_argument("--use_coord_pos_encoding", type=str2bool, nargs='?', const=True, default=False)
+        parser.add_argument("--coord_pos_encoding_multires", type=int, default=10)
         return parent_parser
 
     def cleanup(self):
@@ -479,3 +486,54 @@ def gather_nd(params, indices):
     indices = indices.reshape((num_samples, m)).transpose(0, 1).tolist()
     output = params[indices] 
     return output.reshape(out_shape).contiguous()
+
+
+class Embedder:
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+        self.create_embedding_fn()
+
+    def create_embedding_fn(self):
+        embed_fns = []
+        d = self.kwargs['input_dims']
+        out_dim = 0
+        if self.kwargs['include_input']:
+            embed_fns.append(lambda x : x)
+            out_dim += d
+
+        max_freq = self.kwargs['max_freq_log2']
+        N_freqs = self.kwargs['num_freqs']
+
+        if self.kwargs['log_sampling']:
+            freq_bands = 2.**torch.linspace(0., max_freq, steps=N_freqs)
+        else:
+            freq_bands = torch.linspace(2.**0., 2.**max_freq, steps=N_freqs)
+
+        for freq in freq_bands:
+            for p_fn in self.kwargs['periodic_fns']:
+                embed_fns.append(lambda x, p_fn=p_fn, freq=freq : p_fn(x * freq))
+                out_dim += d
+
+        self.embed_fns = embed_fns
+        self.out_dim = out_dim
+
+    def embed(self, inputs):
+        return torch.cat([fn(inputs) for fn in self.embed_fns], -1)
+
+
+def get_embedder(multires=10):
+    if multires == -1:
+        return nn.Identity(), 3
+
+    embed_kwargs = {
+                'include_input' : False,
+                'input_dims' : 3,
+                'max_freq_log2' : multires-1,
+                'num_freqs' : multires,
+                'log_sampling' : True,
+                'periodic_fns' : [torch.sin, torch.cos],
+    }
+
+    embedder_obj = Embedder(**embed_kwargs)
+    embed = lambda x, eo=embedder_obj : eo.embed(x)
+    return embed, embedder_obj.out_dim
