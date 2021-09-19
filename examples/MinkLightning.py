07e8950fd7fe0702f023175fd4c9ab2db6bb9f76
chuchis <jesus.zarzar@utexas.edu>
2021-09-19 17:42:31 +0300

    Added default pipeline seg head

--- examples/MinkLightning.py
+++ examples/MinkLightning.py
@@ -7,6 +7,8 @@ from examples.str2bool import str2bool
 from examples.basic_blocks import MLP
 from examples.BaseSegLightning import BaseSegmentationModule
 
+import numpy as np
+
 def to_precision(inputs, precision):
     if precision == 16:
         dtype = torch.float16
@@ -19,34 +21,45 @@ def to_precision(inputs, precision):
 class MinkowskiSegmentationModule(BaseSegmentationModule):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.model = MinkUNet34C(self.in_channels, self.out_channels)
+        if self.use_seg_head:
+            self.mlp_channels = [int(i) for i in self.mlp_channels.split(',')]
+            if self.relative_mlp_channels:
+                self.mlp_channels = (self.mlp_extra_in_channels) * np.array(self.mlp_channels)
+                # print(self.mlp_channels)
+            else:
+                self.mlp_channels = [self.mlp_extra_in_channels] + self.mlp_channels
+
+            self.model = MinkUNet34C(self.in_channels, self.mlp_extra_in_channels)
+            seg_head_list = []
+            if self.seg_head_in_bn:
+                seg_head_list.append(norm_layer(norm_type='batch', nc=self.mlp_channels[0]))
+            seg_head_list += [MLP(self.mlp_channels, dropout=self.seg_head_dropout),
+                             nn.Conv1d(self.mlp_channels[-1], self.out_channels, kernel_size=1, bias=True)]
+            self.seg_head = nn.Sequential(*seg_head_list)
+        else:
+            self.model = MinkUNet34C(self.in_channels, self.out_channels)
+
         if self.pretrained_minkunet_ckpt is not None:
             print("Loading checkpoint from ", self.pretrained_minkunet_ckpt)
             pretrained_ckpt = torch.load(self.pretrained_minkunet_ckpt)
             if 'state_dict' in pretrained_ckpt:
                 pretrained_ckpt = pretrained_ckpt['state_dict']
-            del pretrained_ckpt['conv0p1s1.kernel']
-            # del pretrained_ckpt['final.kernel']
-            # del pretrained_ckpt['final.bias']
-            # for name, val in self.model.named_parameters():
-            #     print(name, val)
-            #     break
+            if self.in_channels != 3:
+                del pretrained_ckpt['conv0p1s1.kernel']
+            if self.mlp_extra_in_channels != 20:
+                del pretrained_ckpt['final.kernel']
+                del pretrained_ckpt['final.bias']
             self.model.load_state_dict(pretrained_ckpt, strict=False)
-            # for name, val in self.model.named_parameters():
-            #     print(name, val)
-            #     break
-
-        # if self.use_seg_head:
-        #     self.seg_head = nn.Sequential(MLP(self.mlp_channels, dropout=self.seg_head_dropout),
-        #                               nn.Conv1d(self.mlp_channels[-1], self.out_channels, kernel_size=1, bias=True)
-        #                               # nn.Linear(self.mlp_channels[-1], self.out_channels)
-        #                               )
 
     def forward(self, x):
         # for name, val in self.model.named_parameters():
             # print(name, val)
             # break
-        return self.model(x)
+        out = self.model(x)
+        if self.use_seg_head:
+            # print(out._F.shape)
+            out._F = self.seg_head(out._F.unsqueeze(-1)).squeeze(-1)
+        return out
 
     def training_step(self, batch, batch_idx):
         # print("Train ", batch)
@@ -106,6 +119,11 @@ class MinkowskiSegmentationModule(BaseSegmentationModule):
         parser = parent_parser.add_argument_group("MinkSegModel")
         parser.add_argument("--use_seg_head", type=str2bool, nargs='?', const=True, default=False)
         parser.add_argument("--pretrained_minkunet_ckpt", type=str, default=None)
+        parser.add_argument("--seg_head_in_bn", type=str2bool, nargs='?', const=True, default=False)
+        parser.add_argument('--seg_head_dropout', type=float, default=0.3)
+        parser.add_argument("--mlp_channels", type=str, default='1,4,8,4')
+        parser.add_argument("--relative_mlp_channels", type=str2bool, nargs='?', const=True, default=True)
+        parser.add_argument("--mlp_extra_in_channels", type=int, default=3)
         return parent_parser
 
 
